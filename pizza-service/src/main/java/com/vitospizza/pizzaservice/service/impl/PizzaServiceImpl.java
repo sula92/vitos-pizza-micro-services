582f690a9aa11f0b8654a85fa4dfbca07916eac7
sula92 <sulakkanaid@gmail.com>
2023-07-22 02:22:35 +0530

    add mapToProductResponse method to service

--- pizza-service/src/main/java/com/vitospizza/pizzaservice/service/impl/PizzaServiceImpl.java
+++ pizza-service/src/main/java/com/vitospizza/pizzaservice/service/impl/PizzaServiceImpl.java
@@ -3,6 +3,7 @@ package com.vitospizza.pizzaservice.service.impl;
 import com.vitospizza.pizzaservice.dto.PizzaRequest;
 import com.vitospizza.pizzaservice.dto.PizzaResponse;
 import com.vitospizza.pizzaservice.exception.PizzaNotFoundException;
+import com.vitospizza.pizzaservice.model.Pizza;
 import com.vitospizza.pizzaservice.service.PizzaService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Lazy;
@@ -26,5 +27,14 @@ public class PizzaServiceImpl implements PizzaService {
     @Override
     public PizzaResponse getPizzaById() throws PizzaNotFoundException {
         return null;
+    }
+
+    private PizzaResponse mapToProductResponse(Pizza pizza) {
+        return PizzaResponse.builder()
+                .id(pizza.getId())
+                .name(pizza.getName())
+                .description(pizza.getDescription())
+                .price(pizza.getPrice())
+                .build();
     }
 }
