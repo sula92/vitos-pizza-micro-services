fec097fc95848f12f14b3daa3c05bd6b972fc3c0
sula92 <sulakkanaid@gmail.com>
2023-07-24 02:43:29 +0530

    add placeOrder to controller

--- order-service/src/main/java/com/vitospizza/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/vitospizza/orderservice/controller/OrderController.java
@@ -1,13 +1,32 @@
 package com.vitospizza.orderservice.controller;
 
+import com.vitospizza.orderservice.dto.OrderRequest;
+import com.vitospizza.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
 @Slf4j
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        log.info("Placing Order");
+        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
+    }
 }
