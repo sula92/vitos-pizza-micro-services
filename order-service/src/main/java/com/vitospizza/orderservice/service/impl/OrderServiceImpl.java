ce727ddbd650de15e1d029cee3c9adc41e30394d
sula92 <sulakkanaid@gmail.com>
2023-07-24 03:00:44 +0530

    create mapToDto method

--- order-service/src/main/java/com/vitospizza/orderservice/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/vitospizza/orderservice/service/impl/OrderServiceImpl.java
@@ -1,6 +1,8 @@
 package com.vitospizza.orderservice.service.impl;
 
+import com.vitospizza.orderservice.dto.OrderLineItemsDto;
 import com.vitospizza.orderservice.dto.OrderRequest;
+import com.vitospizza.orderservice.model.OrderLineItems;
 import com.vitospizza.orderservice.service.OrderService;
 
 public class OrderServiceImpl implements OrderService {
@@ -8,5 +10,13 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public String placeOrder(OrderRequest orderRequest) {
         return null;
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
     }
 }
