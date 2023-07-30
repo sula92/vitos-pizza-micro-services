190ef02d08211e1f7d6d1c685ae920e64300bf54
sula92 <sulakkanaid@gmail.com>
2023-07-31 03:39:48 +0530

    remove session service

---
+++ api-gateway/src/main/java/com/vitospizza/apigateway/constants/StringConst.java
@@ -0,0 +1,7 @@
+package com.vitospizza.apigateway.constants;
+
+public class StringConst {
+    public static final String ORDER_SERVICE_URL="";
+    public static final String PIZZA_SERVICE_URL="";
+    public static final String SESSION_SERVICE_URL="";
+}
